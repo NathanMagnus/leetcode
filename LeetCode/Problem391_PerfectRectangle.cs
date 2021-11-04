09d9a2770dfacb077795aabe5c7a80ce2e126c0f
Nathan Magnus <nathan@foodxtech.com>
2021-11-04 07:53:15 -0600

    perfect rectangle

--- LeetCode/Problem391_PerfectRectangle.cs
+++ LeetCode/Problem391_PerfectRectangle.cs
@@ -5,15 +5,17 @@ using System.Linq;
 namespace LeetCode
 {
     // Difficulty: Hard
-    // Time: 
+    // Time: 30min 
     public class Problem391_PerfectRectangle
     {
         [Test]
         [TestCase("[[1,1,3,3],[3,1,4,2],[3,2,4,4],[1,3,2,4],[2,3,3,4]]", true)]
         [TestCase("[[1,1,2,3],[1,3,2,4],[3,1,4,2],[3,2,4,4]]", false)]
-        [TestCase("[[1,1,3,3],[3,1,4,2],[1,3,2,4],[3,2,4,4]]", false)]
+        [TestCase("[[1,1,3,3],[3,1,4,2],[1,3,2,4],[3,2,4,4]]", false)]//
         [TestCase("[[1,1,3,3],[3,1,4,2],[1,3,2,4],[2,2,4,4]]", false)]
-        //[TestCase("", true)]
+        [TestCase("[[0,0,4,1],[0,0,4,1]]", false)]
+        [TestCase("[[0,0,4,1],[7,0,8,3],[5,1,6,3],[6,0,7,2],[4,0,5,1],[4,2,5,3],[2,1,4,3],[0,2,2,3],[0,1,2,2],[6,2,8,3],[5,0,6,1],[4,1,5,2]]", false)]
+        [TestCase("[[7,0,8,3],[6,2,8,3]]", false)]
         public void Test(string s, bool expected)
         {
             var split = s[2..^2]
@@ -34,51 +36,57 @@ namespace LeetCode
                 .Select(points => new Rectangle(points.Item1, points.Item2))
                 .ToArray();
 
-            //TODO: If any rectangles intersect, then false
-            //TODO: For each rect, make sure there is one that picks off where it left off
+            var minX = rectangleObjects.Min(rect => rect.BottomLeft.X);
+            var maxX = rectangleObjects.Max(rect => rect.TopRight.X);
+            var minY = rectangleObjects.Min(rect => rect.BottomLeft.Y);
+            var maxY = rectangleObjects.Max(rect => rect.TopRight.Y);
 
-            //var minX = rectangleObjects.Min(rect => rect.BottomLeft.X);
-            //var maxX = rectangleObjects.Max(rect => rect.TopRight.X);
-            //var minY = rectangleObjects.Min(rect => rect.BottomLeft.Y);
-            //var maxY = rectangleObjects.Max(rect => rect.TopRight.Y);
+            var tiles = new bool[maxX, maxY];
 
-            //for (double x = minX + 0.5; x < maxX; x++)
-            //{
-            //    for (double y = minX + 0.5; y < maxY; y++)
-            //    {
-            //        var inRectangleCount = GetRectangleContainCount(rectangleObjects, new Point(x, y));
-            //        if (IsOnEdge(rectangleObjects, new Point(x, y)) && inRectangleCount > 1)
-            //            inRectangleCount--;
-            //        if (inRectangleCount != 1)
-            //            return false;
-            //    }
-            //}
+            for (var i = 0; i < rectangleObjects.Length; i++)
+            {
+                var intersect = rectangleObjects
+                    .Where(rect => rect != rectangleObjects[i])
+                    .Any(rect => Intersect(rectangleObjects[i], rect));
+                if (intersect)
+                    return false;
+            }
+
+            for (var x = minX; x < maxX; x++)
+            {
+                for (var y = minY; y < maxY; y++)
+                {
+                    var testRect = new Rectangle(new Point(x, y), new Point(x + 1, y + 1));
+                    var covered = rectangleObjects.Any(rect => Intersect(rect, testRect));
+                    if (!covered)
+                        return false;
+                }
+            }
 
             return true;
         }
 
-        private bool IsOnEdge(Rectangle[] rectangleObjects, Point point) 
-            => rectangleObjects
-                .Any(rect => rect.BottomLeft.Y == point.Y || rect.BottomLeft.Y == point.Y || rect.TopRight.X == point.X || rect.TopRight.Y == point.Y);
-
-        private int GetRectangleContainCount(Rectangle[] rectangles, Point point)
-            => rectangles
-            .Where(rect => RectangleContains(rect, point))
-            .Count();
-
-        private bool RectangleContains(Rectangle rect, Point point)
-            => rect.BottomLeft.X <= point.X && rect.TopRight.X >= point.X
-                && rect.BottomLeft.Y <= point.Y && rect.TopRight.Y >= point.Y;
-
-        private Point GetBottomLeft(Rectangle[] rectangleObjects)
+        private bool Intersect(Rectangle rectangle1, Rectangle rectangle2)
         {
-            var minX = rectangleObjects.Min(obj => obj.BottomLeft.X);
-            return rectangleObjects.First(rect => rect.BottomLeft.X == minX).BottomLeft;
-        }
-        private Point GetTopRight(Rectangle[] rectangleObjects)
-        {
-            var maxY = rectangleObjects.Max(obj => obj.TopRight.Y);
-            return rectangleObjects.First(rect => rect.TopRight.Y == maxY).TopRight;
+            if (rectangle1.BottomLeft.X >= rectangle2.TopRight.X || rectangle2.BottomLeft.X >= rectangle1.TopRight.X)
+                return false;
+
+            if (rectangle1.BottomLeft.Y >= rectangle2.TopRight.Y || rectangle2.BottomLeft.Y >= rectangle1.TopRight.Y)
+                return false;
+
+            //bottom left in rect then intersect
+            //if (rectangle1.BottomLeft.X <= rectangle2.BottomLeft.X && rectangle1.TopRight.X > rectangle2.BottomLeft.X //within X range
+            //   && rectangle1.BottomLeft.Y <= rectangle2.BottomLeft.Y && rectangle1.TopRight.Y > rectangle2.BottomLeft.Y // and within Y range
+            //)
+            //    return true; //intersect
+
+            ////top left in rect, then intersect
+            //if (rectangle1.BottomLeft.X < rectangle2.TopLeft.X && rectangle1.TopRight.X >= rectangle2.TopLeft.X //within X range
+            //   && rectangle1.BottomLeft.Y < rectangle2.TopLeft.Y && rectangle1.TopRight.Y >= rectangle2.TopLeft.Y // and within Y range
+            //)
+            //    return true; //intersect
+
+            return true;
         }
 
         private class Rectangle
@@ -99,14 +107,126 @@ namespace LeetCode
 
         private class Point
         {
-            public Point(double x, double y)
+            public Point(int x, int y)
             {
                 X = x;
                 Y = y;
             }
 
-            public double X { get; }
-            public double Y { get; }
+            public int X { get; }
+            public int Y { get; }
+        }
+    }
+
+    public class Problem391_PerfectRectangle_slow
+    {
+        [Test]
+        [TestCase("[[1,1,3,3],[3,1,4,2],[3,2,4,4],[1,3,2,4],[2,3,3,4]]", true)]
+        [TestCase("[[1,1,2,3],[1,3,2,4],[3,1,4,2],[3,2,4,4]]", false)]
+        [TestCase("[[1,1,3,3],[3,1,4,2],[1,3,2,4],[3,2,4,4]]", false)]//
+        [TestCase("[[1,1,3,3],[3,1,4,2],[1,3,2,4],[2,2,4,4]]", false)]
+        [TestCase("[[0,0,4,1],[0,0,4,1]]", false)]
+        [TestCase("[[0,0,4,1],[7,0,8,3],[5,1,6,3],[6,0,7,2],[4,0,5,1],[4,2,5,3],[2,1,4,3],[0,2,2,3],[0,1,2,2],[6,2,8,3],[5,0,6,1],[4,1,5,2]]", false)]
+        [TestCase("[[7,0,8,3],[6,2,8,3]]", false)]
+        public void Test(string s, bool expected)
+        {
+            var split = s[2..^2]
+                .Split("],[")
+                .Select(points => points.Split(","))
+                .Select(items => items.Select(int.Parse).ToArray())
+                .ToArray();
+
+            var sut = new Problem391_PerfectRectangle();
+            var result = sut.IsRectangleCover(split);
+            Assert.AreEqual(expected, result);
+        }
+
+        public bool IsRectangleCover(int[][] rectangles)
+        {
+            var rectangleObjects = rectangles
+                .Select(points => (new Point(points[0], points[1]), new Point(points[2], points[3])))
+                .Select(points => new Rectangle(points.Item1, points.Item2))
+                .ToArray();
+
+            var minX = rectangleObjects.Min(rect => rect.BottomLeft.X);
+            var maxX = rectangleObjects.Max(rect => rect.TopRight.X);
+            var minY = rectangleObjects.Min(rect => rect.BottomLeft.Y);
+            var maxY = rectangleObjects.Max(rect => rect.TopRight.Y);
+
+            var tiles = new bool[maxX, maxY];
+
+            for (var i = 0; i < rectangleObjects.Length; i++)
+            {
+                var intersect = rectangleObjects
+                    .Where(rect => rect != rectangleObjects[i])
+                    .Any(rect => Intersect(rectangleObjects[i], rect));
+                if(intersect)
+                    return false;
+            }
+
+            for (var x = minX; x < maxX; x++)
+            {
+                for (var y = minY; y < maxY; y++)
+                {
+                    var testRect = new Rectangle(new Point(x, y), new Point(x + 1, y + 1));
+                    var covered = rectangleObjects.Any(rect => Intersect(rect, testRect));
+                    if (!covered)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool Intersect(Rectangle rectangle1, Rectangle rectangle2)
+        {
+            if(rectangle1.BottomLeft.X >= rectangle2.TopRight.X || rectangle2.BottomLeft.X >= rectangle1.TopRight.X)
+                return false;
+
+            if(rectangle1.BottomLeft.Y >= rectangle2.TopRight.Y || rectangle2.BottomLeft.Y >= rectangle1.TopRight.Y)
+                return false;
+
+            //bottom left in rect then intersect
+            //if (rectangle1.BottomLeft.X <= rectangle2.BottomLeft.X && rectangle1.TopRight.X > rectangle2.BottomLeft.X //within X range
+            //   && rectangle1.BottomLeft.Y <= rectangle2.BottomLeft.Y && rectangle1.TopRight.Y > rectangle2.BottomLeft.Y // and within Y range
+            //)
+            //    return true; //intersect
+
+            ////top left in rect, then intersect
+            //if (rectangle1.BottomLeft.X < rectangle2.TopLeft.X && rectangle1.TopRight.X >= rectangle2.TopLeft.X //within X range
+            //   && rectangle1.BottomLeft.Y < rectangle2.TopLeft.Y && rectangle1.TopRight.Y >= rectangle2.TopLeft.Y // and within Y range
+            //)
+            //    return true; //intersect
+
+            return true;
+        }
+
+        private class Rectangle
+        {
+            public Rectangle(Point bottomLeft, Point topRight)
+            {
+                BottomLeft = bottomLeft;
+                TopRight = topRight;
+                BottomRight = new Point(TopRight.X, BottomLeft.Y);
+                TopLeft = new Point(BottomLeft.X, TopRight.Y);
+            }
+
+            public Point BottomLeft { get; }
+            public Point TopRight { get; }
+            public Point TopLeft { get; }
+            public Point BottomRight { get; }
+        }
+
+        private class Point
+        {
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; }
+            public int Y { get; }
         }
     }
 }
