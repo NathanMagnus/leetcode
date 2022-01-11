27edd2e9429700821ec81196d9ac0fd8643681a4
Nathan Magnus <nathan@foodxtech.com>
2022-01-11 15:41:42 -0600

    part way through a problem

--- LeetCode/Problem1444_NumberOfWaysOfCuttingAPizza.cs
+++ LeetCode/Problem1444_NumberOfWaysOfCuttingAPizza.cs
@@ -1,18 +1,19 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace LeetCode
 {
-    // Difficulty: 
-    // Time: 
+    // Difficulty: Hard
+    // Time: ??
     public class Problem1444_NumberOfWaysOfCuttingAPizza
     {
         [Test]
         [TestCase("A..|AAA|...", 3, 3)]
         [TestCase("A..|AA.|...", 3, 1)]
-        [TestCase("A..|A..|...", 3, 1)]
+        [TestCase("A..|A..|...", 1, 1)]
         public void Test(string s, int k, int expected)
         {
             var ways = s.ToStringArray('|');
@@ -22,44 +23,57 @@ namespace LeetCode
             Assert.AreEqual(expected, result);
         }
 
+        private readonly int _mod = (int)Math.Pow(10, 9) + 7;
+        private readonly IDictionary<(int Row, int Col, int CutsLeft), int> _memory = new Dictionary<(int Row, int Col, int CutsLeft), int>();
+
         public int Ways(string[] pizza, int k)
         {
-            var grid = new int[pizza.Length][];
-            for (var i = 0; i < pizza.Length; i++)
-                grid[i] = new int[pizza[i].Length];
+            if (k == 0 || pizza.Length + pizza[0].Length < k)
+                return 0;
 
-            var max = 0;
-
-            for (var i = grid.Length - 1; i >= 0; i--)
+            var apples = new int[pizza.Length][];
+            for (var i = pizza.Length - 1; i >= 0; i--)
             {
-                for (var j = grid[i].Length - 1; j >= 0; j--)
+                apples[i] = new int[pizza[i].Length];
+                for (var j = pizza[i].Length - 1; j >= 0; j--)
                 {
-                    var horizontalCutValid = ContainsApple(pizza[..i]) // row and before
-                        && ContainsApple(pizza[(i + 1)..]); // after row
-                    var verticalCutValid = ContainsApple(pizza[i..].Select(row => row[..j])) // column and before
-                        && ContainsApple(pizza[i..].Select(piece => piece[(j + 1)..]).ToArray()); // column and after
-                    if (horizontalCutValid)
-                        grid[i][j]++;
-                    if (verticalCutValid)
-                        grid[i][j]++;
+                    if (i < apples.Length - 1)
+                        apples[i][j] += apples[i + 1][j];
+                    if (j < apples[i].Length - 1)
+                        apples[i][j] += apples[i][j + 1];
+                    if (i < apples.Length - 1 && j < apples[i].Length - 1)
+                        apples[i][j] -= apples[i + 1][j + 1];
+                    if (pizza[i][j] == 'A')
+                        apples[i][j]++;
                 }
             }
 
-            return max;
+            return Cut(pizza, apples, k);
         }
 
-        private bool ContainsApple(IEnumerable<string> pizza)
-            => pizza.Any(row => row.Contains('A'));
+        private int Cut(string[] pizza, int[][] apples, int k)
+        {
+            if (k <= 1)
+                return apples[0][0] > 0 ? 1 : 0;
+            
+            var key = (pizza.Length, pizza[0].Length, k);
+            if(_memory.ContainsKey(key))
+                return _memory[key];
 
-        //private int ComputeCuts(string[] pizza, int row, int col, int k)
-        //{
-        //    for(var i = row; i < pizza.Length; i++)
-        //    {
-        //        for(var j = col; j < pizza[i].Length; j++)
-        //        {
-        //            if()
-        //        }
-        //    }
-        //}
+            var count = 0;
+            for (var i = 1; i < pizza.Length; i++)
+            {
+                if (apples[0][0] - apples[i][0] > 0)
+                    count += Cut(pizza[i..], apples[i..], k - 1);
+            }
+            for (var j = 1; j < pizza[0].Length; j++)
+            {
+                if (apples[0][0] - apples[0][j] > 0)
+                    count += Cut(pizza.Select(r => r[j..]).ToArray(), apples.Select(a => a[j..]).ToArray(), k - 1);
+            }
+
+            _memory[key] = count % _mod;
+            return count;
+        }
     }
 }
--- LeetCode/Template.cs
+++ LeetCode/Template.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LeetCode
@@ -9,17 +10,64 @@ namespace LeetCode
     public class Template
     {
         [Test]
-        [TestCase("bb", "")]
-        public void Test(string s, string expected)
+        [TestCase("-10,10,-10,10", 10000)]
+        [TestCase("0,-4,0,-8", 0)]
+        public void Test(string s, int expected)
         {
             var sut = new Template();
-            var result = sut.MethodName(s);
+            var result = sut.maxProduct(s.ToIntArray().ToList());
             Assert.AreEqual(expected, result);
         }
 
-        public string MethodName(string s)
+        public class Holder
         {
-            return String.Empty;
+            public int Max { get; set; }
+            public int Min { get; set; }
+        }
+
+        public int maxProduct(List<int> A)
+        {
+            // max product of i to j for all i<j
+            // 2, 3
+            // 2, 3, -2     3, -2     -2
+            var max = int.MinValue;
+            var min = int.MaxValue;
+            var store = new Holder[A.Count];
+
+            for (var i = 0; i < A.Count; i++)
+            {
+                var localMin = A[i]; // 10
+                var localMax = A[i]; // 10
+                if (i > 0)
+                {
+                    var small = A[i] * store[i - 1].Min; // -100 * 10 = -1000
+                    var large = A[i] * store[i - 1].Max; // 1000 * 10 = 10000
+                    localMax = Math.Max(localMax, Math.Max(small, large)); // 10000
+                    localMin = Math.Min(localMin, Math.Min(small, large)); // -1000
+                }
+
+                //localMax = Math.Max(localMax, localMax * A[i]); // 10000
+                //localMin = Math.Min(localMin, localMin * A[i]); // -1000
+                store[i] = new Holder() { Max = localMax, Min = localMin }; // 10000 -1000
+                max = Math.Max(localMax, max); // 10000
+            }
+
+
+            // var store = new int[A.Count][];
+            // for(var i = 0; i < A.Count; i++)
+            // {
+            //     var product = 1;
+            //     store[i] = new int[A.Count];
+            //     for (var j = i; j < A.Count; j++)
+            //     {
+            //         product *= A[j];
+            //         store[i][j] = product;
+            //         max = Math.Max(max, product);
+            //     }
+            // }
+
+            return max;
         }
     }
+
 }
